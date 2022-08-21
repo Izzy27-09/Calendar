6ff2f045a17b714cc83e491852fee251b7e227a6
Izzy27-09 <88539501+Izzy27-09@users.noreply.github.com>
2022-08-21 17:56:25 -0400

    Update Calender.cpp

--- Calender.cpp
+++ Calender.cpp
@@ -8,7 +8,23 @@ int main()
     int choice;
     std::cout << "1.Set Date\n";
     std::cout << "2.Display Date(mm/dd/yyyy)\n";
-    std::cout<<"3 Display Date(Weekday,Month day,Year\n";
+    std::cout<<"3 Display Date(Weekday,Month, day,Year\n";
     std::cin >> choice;
+    std::cout << "\n";
+    switch (choice) {
+    case 1:
+        cal.SetYear();
+        cal.SetMonth();
+        cal.SetDay();
+        break;
+    case 2:
+        std::cout << cal.GetMonth(); std::cout << "/"; std::cout << cal.GetDay(); std::cout << "/"; std::cout << cal.GetYear();
+        break;
+    case 3:
+        cal.ExactDate();
+        break;
+        
+    }
 }
+
 
